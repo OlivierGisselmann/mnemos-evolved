49c1648897bef4dc19ae59f83296c9905280f4e3
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-07-08 19:00:18 +0200

    feat: add platform agnostic typedefs

---
+++ engine/platform/types.hpp
@@ -0,0 +1,24 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <cstdint>
+
+namespace mnm
+{
+    typedef     std::int8_t             i8;
+    typedef     std::int16_t            i16;
+    typedef     std::int32_t            i32;
+    typedef     std::int64_t            i64;
+
+    typedef     std::uint8_t            u8;
+    typedef     std::uint16_t           u16;
+    typedef     std::uint32_t           u32;
+    typedef     std::uint64_t           u64;
+
+    typedef     float                   f32;
+    typedef     double                  f64;
+
+    typedef     std::size_t             sizet;
+}
+
+#endif
