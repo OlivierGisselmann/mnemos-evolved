f6c82ba52a69edd8d435a2ef94a5e60579bb0cdd
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-07-23 23:58:57 +0200

    feat: add renderer abstract class

--- engine/renderer/renderer.hpp
+++ engine/renderer/renderer.hpp
@@ -1,14 +1,17 @@
 #ifndef RENDERER_HPP
 #define RENDERER_HPP
 
-#include <glad/glad.h>
-
 namespace mnm::renderer
 {
-    void InitRenderer();
-    void BeginFrame();
-    void RenderFrame();
-    void EndFrame();
+    class IRenderer
+    {
+    public:
+        virtual void BeginFrame() = 0;
+        virtual void DrawFrame() = 0;
+        virtual void EndFrame() = 0;
+
+        virtual ~IRenderer() = default;
+    };
 }
 
 #endif
